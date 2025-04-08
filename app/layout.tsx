68e6cf67496b026592dffcc7ac47889bedb277fd
ali raza <alirazahaider079@gmail.com>
2025-04-08 21:57:31 +0500

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Real Estate ROI Calculator",
+  title: "Real Estate ROI Calculator | Ali Raza",
   description: "Calculate your real estate investment return on investment (ROI) easily with our user-friendly calculator. Get instant results and insights into your property investments.",
 };
 
